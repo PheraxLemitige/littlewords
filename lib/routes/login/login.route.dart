b6fb350e8132dfe7c1bdf72efab33be45cac09ce
AkumaCipher <petitmamass59@gmail.com>
2023-03-01 14:28:12 +0100

    Updated app.dart and login.route.dart

--- lib/routes/login/login.route.dart
+++ lib/routes/login/login.route.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:littlewords/widgets/littleworlds_logo.dart';
 
 class LoginRoute extends StatelessWidget {
-  const LoginRoute({Key? key}) : super(key: key);
+  LoginRoute({Key? key}) : super(key: key);
+
+  final TextEditingController _txtController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +15,33 @@ class LoginRoute extends StatelessWidget {
         LittleWorldsLogo(),
         Spacer(),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal:8.0),
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
           child: TextField(
+            controller: _txtController,
               decoration: InputDecoration(
             fillColor: Colors.lightBlue,
             filled: true,
           )),
         ),
         Padding(
-          padding: const EdgeInsets.only(top:8.0, left:8.0, right:8.0,bottom:24),
-          child: ElevatedButton(onPressed: null, style: ElevatedButton.styleFrom(
-            minimumSize: Size.fromHeight(48),
-          ), child: Text("Enregistrer nom"),),
+          padding: const EdgeInsets.only(
+              top: 8.0, left: 8.0, right: 8.0, bottom: 24),
+          child: ElevatedButton(
+            onPressed: _onPressed,
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.fromHeight(48),
+            ),
+            child: Text("Enregistrer nom"),
+          ),
         ),
       ]),
     );
   }
+
+  void _onPressed(){
+    var text = _txtController.text;
+    print('Text:$text');
+  }
 }
+
+
